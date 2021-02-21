6cc315d7088eb20b1d05e61f76e97e25d7942fd0
Nora <nora_calif101@outlook.com>
2021-02-21 10:51:05 -0800

    Update inventory.py

--- unitTwoProject/inventory.py
+++ unitTwoProject/inventory.py
@@ -9,18 +9,20 @@ class Inventory:
                 - You won't receive damage from the hits; only the Warrior will
             Ninja mist: Hides your movement from the Trickster
                 - They'll perceive your movement as a retreat so they'll attack (w/ less damage than you)
-            Cursed sabotage: Makes it so that the Wizard takes 10 damage for using magic
+            Cursed sabotage: Makes it so that the Wizard takes 20 damage for using magic
                 - This includes; magic attacks (potions & spells), magic defense (shield), normal defense,
                   and strong attacks (everything but normal 7 damage attacks)
                 - They can still use magic, but it'll be detrimental for them
 
-        2. General weapons: Items that can be accessed during any battle
-        [Work for 2 turns and can be purchased 3 times]
-            ADD ITEMS
-
-        3. Defensive tools: Items that can be used to replenish health/energy, add shield, or increase damage
-        [One-time purchase & occurrence OR lasts for the rest of the battle (for damage)]
-            ADD ITEMS
+        2. General tools: Items that can be accessed during any battle
+            Protein drink: Increases energy to 100%
+                - One-time occurrence; 2 available
+            Enchanted sword: Increases damage to x2 (+ has a unique attack description!)
+                - Applicable for 3 turns; 2 available
+            Force field: Increases shield by 50
+                - One-time occurrence; 2 available
+            Bandages: Increases health by 20
+                - One-time occurrence; 2 available
 
     '''
 
