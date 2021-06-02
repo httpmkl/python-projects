665f0e54f028ebb98578a9eb3fefb5830ead6eab
Nora <nora_calif101@outlook.com>
2021-06-01 18:25:19 -0700

    Update main.py

--- textEditor/main.py
+++ textEditor/main.py
@@ -18,7 +18,10 @@ def menu():
 
 def checkForErrors(rawText):
     filteredText = separateWordsProperly(rawText)
-    print(filteredText)
+    filText = str(filteredText)
+    samp = filText.split(' ')
+    samp.pop()
+    print(samp)
 
 
 def separateWordsProperly(rawText):
@@ -33,13 +36,11 @@ def separateWordsProperly(rawText):
             word.append(j)
         letters.append(word)
 
-
     for num in range(len(letters)):
         i = list(letters[num])
         for j in range(len(i)):
             if i[j] not in alphabet:
                 letters[num].remove(i[j])
-
 
     for i in letters:
         word = ''
@@ -63,6 +64,7 @@ def getAlphabet():
 
     return alphabet
 
+
 def getDictionaryWords():
     dictionary = FileRead.getFileAllLines(FileRead, 'AllWords.txt')
 
@@ -75,41 +77,56 @@ def getDictionaryWords():
 
 
 def testForAccuracy(incWords):
+    corWords = []
     dictionary = getDictionaryWords()
     for num in range(len(dictionary)):
         word = dictionary[num]
         dictionary[num] = word.lower()
 
     for i in incWords:
-        lettersOne = list(i)
+        options = []
         for j in dictionary:
-            lettersTwo = list(j)
+            if i.lower() != j.lower():
+                lettersOne = list(i)
+                lettersTwo = list(j)
 
-            try:
-                matchingLetters = 0
-                for num in range(len(lettersOne)):
-                    letterOne = lettersOne[num]
-                    letterTwo = lettersTwo[num]
-                    if letterOne.lower() == letterTwo.lower():
-                        matchingLetters += 1
-            except IndexError:
-                pass
+                try:
+                    matchingLetters = 0
+                    for num in range(len(lettersOne)):
+                        letterOne = lettersOne[num]
+                        letterTwo = lettersTwo[num]
+                        if letterOne.lower() == letterTwo.lower():
+                            matchingLetters += 1
+                except IndexError:
+                    pass
 
-            if len(lettersOne) <= 10:
-                diff = 1
+                if len(lettersOne) <= 13:
+                    diff = 1
+                else:
+                    diff = 2
+
+                maxLetters = len(lettersOne) + diff
+                minLetters = len(lettersOne) - diff
+
+                if len(lettersOne) == len(lettersTwo):
+                    if maxLetters >= matchingLetters >= minLetters:
+                        options.append(j)
             else:
-                diff = 2
+                options.clear()
+                options.append('1')
+                options.append(j)
 
-            maxLetters = len(lettersOne) + diff
-            minLetters = len(lettersOne) - diff
+        if options[0] == '1':
+            answer = options[1]
+            options.clear
+            options.append(answer)
 
-            if len(lettersOne) == len(lettersTwo):
-                if maxLetters >= matchingLetters >= minLetters:
-                        print(i)
-                        print(j)
-                        print()
+        corWords.append(options)
+
+    print(incWords)
+    print(corWords)
 
 
-incWords = ['wondeing', 'hetlo', 'hapxy', 'abuut', 'welcone']
-#testForAccuracy(incWords)
-menu()
+incWords = ['wondeing', 'there', 'hetlo', 'hapxy', 'abuut', 'welcone']
+testForAccuracy(incWords)
+# menu()
