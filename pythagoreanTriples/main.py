eb6f5787a9e9a4c866f057f014392d71d3751097
Nora <nora_calif101@outlook.com>
2022-12-27 16:44:59 -0800

    Added graphing capabilities

--- pythagoreanTriples/main.py
+++ pythagoreanTriples/main.py
@@ -9,8 +9,10 @@ Made by Nora Calif, 2022
 
 import math
 from math import gcd
+import matplotlib.pyplot as plt
+import numpy as np
 
-def py_triples(values):
+def py_triples(values, repeats, multiples):
     "Finds all Pythagorean triples under a given range"
     nums = list(range(1, int(values) + 1))
     notDone = True
@@ -23,18 +25,27 @@ def py_triples(values):
                 b = int(nums[i])
                 c = math.sqrt(math.pow(a,2) + math.pow(b,2)) # Sqrt of a^2 + b^2
                 if c.is_integer(): # If c ends in .0 (pythagorean triple)
-                    if a < b: # To avoid repeats, for ex. 3,4,5 & 4,3,5
+                    if repeats and multiples:
+                        triples.append([a, b, int(c)])  # Adds combo to list of triples
+                    elif repeats:
                         if gcd(a, b) == 1: # To search for primitive triples
                             triples.append([a, b, int(c)]) # Adds combo to list of triples
+                    elif multiples:
+                        if a < b:  # To avoid repeats, for ex. 3,4,5 & 4,3,5
+                            triples.append([a, b, int(c)])  # Adds combo to list of triples
+                    else:
+                        if gcd(a, b) == 1:  # To search for primitive triples
+                            if a < b:  # To avoid repeats, for ex. 3,4,5 & 4,3,5
+                                triples.append([a, b, int(c)])  # Adds combo to list of triples
         break
 
     return triples # Returns the list of pythagorean triples
 
 
-def num_of_triples(values):
+def num_of_triples(values, repeats, multiples):
     "Prints the number of pythagorean triples in a given range"
 
-    print(len(py_triples(int(values)))) # Prints length of triples at given range
+    print(len(py_triples(values, repeats, multiples))) # Prints length of triples at given range
     # Decent speed for a range of numbers <1000
 
 
@@ -44,11 +55,26 @@ def superscript(n):
     return "".join(["⁰¹²³⁴⁵⁶⁷⁸⁹"[ord(c)-ord('0')] for c in str(n)])
 
 
-def print_triples(values):
+def print_triples(values, repeats, multiples):
     "Prints the equations for Pythagorean triples under a given range"
-    triples = py_triples(int(values))
+    triples = py_triples(values, repeats, multiples)
 
     for i in range(0, len(triples)):
         triple = triples[i] # Separates each Pythagorean triple into it's own list
         print(str(triple[0]) + superscript(2) + " + " + str(triple[1]) + superscript(2) + " = " + str(triple[2]) + superscript(2))
         # Prints the equation for each Pythagorean triple
+
+def graph_triples(values, repeats, multiples):
+    triples = py_triples(values, repeats, multiples)
+    x = []
+    y = []
+
+    for i in range(0, len(triples)):
+        triple = triples[i]
+        x.append(triple[0])
+        y.append(triple[1])
+
+    plt.scatter(x, y, s=2)
+    plt.show()
+
+graph_triples(1000, True, True)
